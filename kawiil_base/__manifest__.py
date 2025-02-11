729e38a30c7994f02b276c57f4ac4a3f81454efc
fsrs-odoo <fsrs@odoo.com>
2025-02-11 00:40:39 +0000

    [FIX] Manufacturing Module NAme.

--- kawiil_base/__manifest__.py
+++ kawiil_base/__manifest__.py
@@ -6,7 +6,7 @@
     'author': 'Odoo, Inc.',
     'website': 'https://github.com/odoo-trainings/kawiil-base/',
     'version': '18.0.1.0.0',
-    'depends': ['sale_management', 'website', 'manufacturing', 'contacts'],
+    'depends': ['sale_management', 'website', 'mrp', 'contacts'],
     'data': [
         'data/product_data.xml',
         'data/partner_data.xml',
